9d1af202b64b78149d3acc9b443cbfffac6ed1ef
Zhiyun <zhiyunguo@hengtiansoft.com>
2016-02-02 16:51:10 +0800

    Initial Commit

---
+++ ZYSectionIndexView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYSectionIndexView
+//
+//  Created by Zhiyun on 16/2/2.
+//  Copyright © 2016年 Zhiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
